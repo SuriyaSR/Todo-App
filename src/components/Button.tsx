10ba4084b850e4775d3fbecd83c91c7688ad02b5
SuriyaSR <suriyasun19@gmail.com>
2025-05-22 23:39:26 +0530

    UI changes and State updation for each todos with working delete button

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+
+export default function Button({buttonType, children}) {
+    
+  return (
+    <button className={`h-[45px] bg-[#473a2b] w-full text-white rounded-[5px] hover:bg-[#322618] ${buttonType === 'secondary' ? 'opacity-[85%]' : ''}`} >
+        {children}
+    </button>
+  )
+}
