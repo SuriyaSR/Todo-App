83ca4a95f3cf4ddd830c0410d3bf7d368b8b5a07
SuriyaSR <suriyasun19@gmail.com>
2025-05-23 23:21:50 +0530

    completed add, delete, toggle tasks and improve app structure

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import Button from "./Button";
 
-export default function AddTodoForm() {
+export default function AddTodoForm({todos, handleAddTodo}) {
+  
+  const [todoText, setTodoText] = useState('');
+  
   return (
-    <form action="">
+    <form onSubmit={(e) => {
+        e.preventDefault();  
+        handleAddTodo(todoText);      
+        setTodoText('');    
+    }}>
         <h2 className="font-medium text-[#231d15]">Add a Todo</h2>
-        <input type="text" className="h-[45px] border border-black/[12%] rounded-[5px]  my-[9px] text-[14px] block w-full px-[16px]"/>
+        <input type="text" className="h-[45px] border border-black/[12%] rounded-[5px]  my-[9px] text-[14px] block w-full px-[16px]" value={todoText} onChange={(e) => {
+            setTodoText(e.target.value);
+        }}/>
         <Button>Add to List</Button>
     </form>
   )
